16d4290d5430a3ec9e0735b9d586c54c3cdd1775
PARK Yong Seo <j@saro.me>
2019-07-22 03:57:47 +0900

    init

---
+++ src/main/java/me/saro/example/msazuuleureka/MsaZuulEurekaApplication.java
@@ -0,0 +1,13 @@
+package me.saro.example.msazuuleureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaZuulEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaZuulEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/saro/example/msazuuleureka/MsaZuulEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package me.saro.example.msazuuleureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsaZuulEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
