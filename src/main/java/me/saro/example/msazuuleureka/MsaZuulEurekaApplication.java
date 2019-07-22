639057966816baba09f53cf4c68ec40b5c852c68
PARK Yong Seo <j@saro.me>
2019-07-22 15:34:52 +0900

    enable retry

--- src/main/java/me/saro/example/msazuuleureka/MsaZuulEurekaApplication.java
+++ src/main/java/me/saro/example/msazuuleureka/MsaZuulEurekaApplication.java
@@ -2,16 +2,20 @@ package me.saro.example.msazuuleureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
+@EnableRetry
 public class MsaZuulEurekaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsaZuulEurekaApplication.class, args);
 	}
-
 }
